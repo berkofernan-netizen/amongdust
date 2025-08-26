3c972d8d7710cce1511a10a91ed798dad6bcbb76
berkofernan-netizen <berkofernan@gmail.com>
2025-08-26 10:39:06 +0300

    Update info3.js

--- assets/environment/info3.js
+++ assets/environment/info3.js
@@ -1,1 +1,1 @@
-// This folder contains vent closing animation and when a character is ejected, ejection_background.png is the picture that symbols background of ejection screen
+// This folder contains vent closing animation and when a character is ejected, ejection_background.png is the picture that is sign of background of ejection screen
