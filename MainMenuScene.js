97135a5782f40aef8d61da060107f832a04ca1e2
berkofernan-netizen <berkofernan@gmail.com>
2025-08-27 21:16:33 +0300

    Create MainMenuScene.js

---
+++ MainMenuScene.js
@@ -0,0 +1,48 @@
+export default class MainMenuScene extends Phaser.Scene {
+    constructor() {
+        super({ key: 'MainMenuScene' });
+    }
+
+    preload() {
+        // Arka plan video ve görselleri yükle
+        this.load.video('introVideo', 'assets/intro.mp4', 'loadeddata', false, true);
+        this.load.image('logo', 'assets/logo.png');
+        this.load.image('playButton', 'assets/ui/play_button.png');
+        this.load.image('localButton', 'assets/ui/local_button.png');
+        this.load.image('settingsButton', 'assets/ui/settings_button.png');
+    }
+
+    create() {
+        // Video arka plan
+        let bgVideo = this.add.video(this.scale.width / 2, this.scale.height / 2, 'introVideo');
+        bgVideo.setLoop(true);
+        bgVideo.play(true);
+        bgVideo.setDisplaySize(this.scale.width, this.scale.height);
+        bgVideo.setDepth(-1); // en arkada kalsın
+
+        // Logo
+        let logo = this.add.image(this.scale.width / 2, this.scale.height / 4, 'logo');
+        logo.setOrigin(0.5);
+
+        // Butonlar
+        let playBtn = this.add.image(this.scale.width / 2, this.scale.height / 2, 'playButton').setInteractive();
+        let localBtn = this.add.image(this.scale.width / 2, this.scale.height / 2 + 80, 'localButton').setInteractive();
+        let settingsBtn = this.add.image(this.scale.width / 2, this.scale.height / 2 + 160, 'settingsButton').setInteractive();
+
+        // Play tıklama
+        playBtn.on('pointerdown', () => {
+            console.log("Play clicked");
+            this.scene.start('LobbyScene'); // İleride Lobby sahnesi gelecek
+        });
+
+        // Local tıklama
+        localBtn.on('pointerdown', () => {
+            console.log("Local clicked");
+        });
+
+        // Settings tıklama
+        settingsBtn.on('pointerdown', () => {
+            console.log("Settings clicked");
+        });
+    }
+                               }
