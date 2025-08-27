c3e13ee517c477c7d76772ba4c3e4307293cb726
berkofernan-netizen <berkofernan@gmail.com>
2025-08-27 23:00:54 +0300

    Update MainMenuScene.js

--- scene/MainMenuScene.js
+++ scene/MainMenuScene.js
@@ -4,8 +4,6 @@ export default class MainMenuScene extends Phaser.Scene {
     }
 
     preload() {
-        // Arka plan video ve görselleri yükle
-        this.load.video('introVideo', 'assets/intro.mp4', 'loadeddata', false, true);
         this.load.image('logo', 'assets/logo.png');
         this.load.image('playButton', 'assets/ui/play_button.png');
         this.load.image('localButton', 'assets/ui/local_button.png');
@@ -13,27 +11,20 @@ export default class MainMenuScene extends Phaser.Scene {
     }
 
     create() {
-    // Video arka plan
-    let bgVideo = this.add.video(this.scale.width / 2, this.scale.height / 2, 'introVideo');
-    bgVideo.setDisplaySize(this.scale.width, this.scale.height);
-    bgVideo.setDepth(-1);
+        // Arka plan (sadece renk dolduralım test için)
+        this.cameras.main.setBackgroundColor('#222244');
 
-    // Sessiz yap → autoplay için
-    bgVideo.setMute(true);
-    bgVideo.play(true);
-    bgVideo.setLoop(true);
+        // Logo
+        let logo = this.add.image(this.scale.width / 2, this.scale.height / 4, 'logo');
+        logo.setOrigin(0.5);
 
-    // Logo
-    let logo = this.add.image(this.scale.width / 2, this.scale.height / 4, 'logo');
-    logo.setOrigin(0.5);
+        // Butonlar
+        let playBtn = this.add.image(this.scale.width / 2, this.scale.height / 2, 'playButton').setInteractive();
+        let localBtn = this.add.image(this.scale.width / 2, this.scale.height / 2 + 80, 'localButton').setInteractive();
+        let settingsBtn = this.add.image(this.scale.width / 2, this.scale.height / 2 + 160, 'settingsButton').setInteractive();
 
-    // Butonlar
-    let playBtn = this.add.image(this.scale.width / 2, this.scale.height / 2, 'playButton').setInteractive();
-    let localBtn = this.add.image(this.scale.width / 2, this.scale.height / 2 + 80, 'localButton').setInteractive();
-    let settingsBtn = this.add.image(this.scale.width / 2, this.scale.height / 2 + 160, 'settingsButton').setInteractive();
-
-    playBtn.on('pointerdown', () => this.scene.start('DropshipLobbyScene'));
-    localBtn.on('pointerdown', () => console.log("Local clicked"));
-    settingsBtn.on('pointerdown', () => console.log("Settings clicked"));
-        }
-                               }
+        playBtn.on('pointerdown', () => {
+            this.scene.start('DropshipLobbyScene');
+        });
+    }
+}
