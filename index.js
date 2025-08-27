b8aa6e33d00383757792bed7f7cfdf73fd7f7075
berkofernan-netizen <berkofernan@gmail.com>
2025-08-27 22:34:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 window.onerror = function(message, source, lineno, colno, error) {
     alert("Error: " + message + "\nLine: " + lineno);
 };
-import MainMenuScene from 'amongdust/scene/MainMenuScene.js';
-import DropshipLobbyScene from 'amongdust/scene/DropshipLobbyScene.js';
-import GameScene from 'amongdust/scene/GameScene.js';
+import MainMenuScene from './scene/MainMenuScene.js';
+import DropshipLobbyScene from './scene/DropshipLobbyScene.js';
+import GameScene from './scene/GameScene.js';
 
 const config = {
     type: Phaser.AUTO,
