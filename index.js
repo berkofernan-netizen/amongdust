c26b368a7d94f6e58a9b002ae38e5fcc8fe72708
berkofernan-netizen <berkofernan@gmail.com>
2025-08-27 23:07:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+window.onerror = function(message, source, lineno, colno, error) {
+  let div = document.createElement("div");
+  div.style.position = "absolute";
+  div.style.top = "0";
+  div.style.left = "0";
+  div.style.background = "black";
+  div.style.color = "red";
+  div.style.padding = "10px";
+  div.style.zIndex = "9999";
+  div.innerText = "Error: " + message + "\nLine: " + lineno + "\nFile: " + source;
+  document.body.appendChild(div);
+};
 import MainMenuScene from './scene/MainMenuScene.js';
 import DropshipLobbyScene from './scene/DropshipLobbyScene.js';
 import GameScene from './scene/GameScene.js';
