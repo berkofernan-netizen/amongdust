2d5ede8e026587f183657f3fcb81ddd5151fdc79
berkofernan-netizen <berkofernan@gmail.com>
2025-08-28 00:03:35 +0300

    Update MainMenuScene.js

--- scene/MainMenuScene.js
+++ scene/MainMenuScene.js
@@ -10,33 +10,25 @@ export default class MainMenuScene extends Phaser.Scene {
     this.load.image('playButton', 'assets/ui/play_button.png');
     this.load.image('localButton', 'assets/ui/local_button.png');
     this.load.image('settingsButton', 'assets/ui/settings_button.png');
+    this.load.video('introVideo', 'assets/intro.mp4', 'canplaythrough', false, true);
   }
 
   create() {
-    // Video arka plan
-    let bgVideo = this.add.video(this.scale.width / 2, this.scale.height / 2, 'menuBg');
+    let bgVideo = this.add.video(this.scale.width / 2, this.scale.height / 2, 'introVideo');
     bgVideo.setDisplaySize(this.scale.width, this.scale.height);
     bgVideo.setDepth(-1);
-    bgVideo.setMute(true);   // Sessiz başlat (autoplay için gerekli)
-    bgVideo.play(true);      // Oynat
-    bgVideo.setLoop(true);   // Sürekli tekrar etsin
 
-    // Logo
-    let logo = this.add.image(this.scale.width / 2, this.scale.height / 4, 'logo');
-    logo.setOrigin(0.5);
-
-    // Butonlar
-    let playBtn = this.add.image(this.scale.width / 2, this.scale.height / 2, 'playButton').setInteractive();
-    let localBtn = this.add.image(this.scale.width / 2, this.scale.height / 2 + 80, 'localButton').setInteractive();
-    let settingsBtn = this.add.image(this.scale.width / 2, this.scale.height / 2 + 160, 'settingsButton').setInteractive();
-
-    // Oyun başlat
-    playBtn.on('pointerdown', () => {
-      this.scene.start('DropshipLobbyScene');
+    // Sessiz başlat
+    this.input.once('pointerdown', () => {
+        bgVideo.setMute(true);
+        bgVideo.play(true);
+        bgVideo.setLoop(true);
     });
 
-    // Diğer butonlar
-    localBtn.on('pointerdown', () => console.log("Local clicked"));
-    settingsBtn.on('pointerdown', () => console.log("Settings clicked"));
-  }
+    // Test için fallback (video açılmazsa kırmızı ekran verelim)
+    bgVideo.on('error', () => {
+        this.cameras.main.setBackgroundColor('#ff0000');
+        console.error("Video oynatılamadı!");
+    });
+      }
                               }
