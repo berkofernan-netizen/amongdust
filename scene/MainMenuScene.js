e5fd3e3e584717aaaee12dd9ab19d3c4a21594e4
berkofernan-netizen <berkofernan@gmail.com>
2025-08-27 23:14:56 +0300

    Update MainMenuScene.js

--- scene/MainMenuScene.js
+++ scene/MainMenuScene.js
@@ -1,7 +1,11 @@
 export default class MainMenuScene extends Phaser.Scene {
   constructor() { super({ key: 'MainMenuScene' }); }
 
+  preload() {
+    this.load.image('logo', 'assets/logo.png');
+  }
+
   create() {
-    this.add.text(100, 100, "Main Menu Scene Çalışıyor!", { font: '32px Arial', fill: '#fff' });
+    this.add.image(this.scale.width/2, this.scale.height/2, 'logo');
   }
 }
