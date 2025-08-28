771cf68e0916df02b8ee90b85d318db559dbcd0e
berkofernan-netizen <berkofernan@gmail.com>
2025-08-28 16:10:59 +0300

    Update GameScene.js

--- scene/GameScene.js
+++ scene/GameScene.js
@@ -4,36 +4,40 @@ export default class GameScene extends Phaser.Scene {
     }
 
     preload() {
-        // Skeld haritası
-        this.load.image('skeldMap', 'assets/map.png');  // map.png yolunu dikkat et
+        // Harita
+        this.load.image('skeldMap', 'assets/map.png'); 
 
-        // Karakter
+        // Oyuncu
         this.load.image('player', 'assets/character/idle.png');
     }
 
     create() {
-        // Haritayı ortala
-        this.add.image(this.scale.width / 2, this.scale.height / 2, 'skeldMap')
-            .setDisplaySize(this.scale.width, this.scale.height);
+        // Haritayı ekle (ortalanmış şekilde)
+        this.map = this.add.image(this.scale.width / 2, this.scale.height / 2, 'skeldMap');
+        this.map.setDisplaySize(this.scale.width, this.scale.height);
 
-        // Oyuncu
+        // Oyuncuyu ekle
         this.player = this.add.sprite(this.scale.width / 2, this.scale.height / 2, 'player');
 
-        // Basit test için klavye kontrolü (ilerde joystick ekleriz)
+        // Klavye kontrolleri
         this.cursors = this.input.keyboard.createCursorKeys();
     }
 
     update() {
+        if (!this.cursors) return;
+
+        let speed = 2;
+
         if (this.cursors.left.isDown) {
-            this.player.x -= 2;
+            this.player.x -= speed;
         } else if (this.cursors.right.isDown) {
-            this.player.x += 2;
+            this.player.x += speed;
         }
 
         if (this.cursors.up.isDown) {
-            this.player.y -= 2;
+            this.player.y -= speed;
         } else if (this.cursors.down.isDown) {
-            this.player.y += 2;
+            this.player.y += speed;
         }
     }
-              }
+                }
