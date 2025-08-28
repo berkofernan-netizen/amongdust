6dc2ab748181f104af1c768f329f2304bd8e3009
berkofernan-netizen <berkofernan@gmail.com>
2025-08-28 13:01:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import MainMenuScene from './scene/MainMenuScene.js';
+import DropshipLobbyScene from './scene/DropshipLobbyScene.js';
+import GameScene from './scene/GameScene.js';
 
 const config = {
     type: Phaser.AUTO,
     width: 1280,
     height: 720,
-    scene: [ MainMenuScene ], // sadece bu sahne
+    scene: [ MainMenuScene, DropshipLobbyScene, GameScene ], // hepsini ekle
     parent: 'game-container'
 };
 
