6aefc45a81461b705b9a9c991c2bf800e9d9bf52
berkofernan-netizen <berkofernan@gmail.com>
2025-08-28 16:09:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-// Hata yakalama kodu (en üstte olmalı)
-window.onerror = function (message, source, lineno, colno, error) {
-  const div = document.createElement("div");
-  div.style.position = "absolute";
-  div.style.top = "0";
-  div.style.left = "0";
-  div.style.background = "red";
-  div.style.color = "white";
-  div.style.padding = "10px";
-  div.style.zIndex = "9999";
-  div.innerText = "Error: " + message + "\nLine: " + lineno;
-  document.body.appendChild(div);
-};
 
 // Importlar sonra gelir
 import MainMenuScene from './scene/MainMenuScene.js';
