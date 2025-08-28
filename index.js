8561cb9692896f2224420fc118fd174c2552ea6e
berkofernan-netizen <berkofernan@gmail.com>
2025-08-28 15:55:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
+// Hata yakalama kodu (en üstte olmalı)
+window.onerror = function (message, source, lineno, colno, error) {
+  const div = document.createElement("div");
+  div.style.position = "absolute";
+  div.style.top = "0";
+  div.style.left = "0";
+  div.style.background = "red";
+  div.style.color = "white";
+  div.style.padding = "10px";
+  div.style.zIndex = "9999";
+  div.innerText = "Error: " + message + "\nLine: " + lineno;
+  document.body.appendChild(div);
+};
+
+// Importlar sonra gelir
 import MainMenuScene from './scene/MainMenuScene.js';
 import DropshipLobbyScene from './scene/DropshipLobbyScene.js';
 import GameScene from './scene/GameScene.js';
 
+// Phaser config
 const config = {
     type: Phaser.AUTO,
     width: 1280,
     height: 720,
-    scene: [ MainMenuScene, DropshipLobbyScene, GameScene ], // hepsi eklendi
+    scene: [ MainMenuScene, DropshipLobbyScene, GameScene ],
     parent: 'game-container'
 };
 
